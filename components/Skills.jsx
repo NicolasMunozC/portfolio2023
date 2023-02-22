2ff5c3ea62c883137e9e8250d4a047dea2cffb67
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-22 13:27:26 -0300

    feat(skills): add skills component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import translations from '@/public/translations'
+import { Box, Heading } from '@chakra-ui/react'
 
-function Skills() {
+function Skills({lang}) {
   return (
-    <div>Skills</div>
+    <Box w='100%' h='100dvh' display='grid' placeContent='center' className='aboutMe sliderMandatoryStart' hidden={true}>
+    <Box mx='30px' pb='5rem'>
+        <Box display='flex' flexDir='row' mb='1rem' className='sliderMandatoryMobile'>
+            <Heading fontSize={['5xl', '5xl', '5xl', '7xl']} fontWeight='thin'>{translations[lang].skills_title}</Heading>
+        </Box>
+        <Box>
+            
+        </Box>
+        </Box>
+    </Box>
   )
 }
 
