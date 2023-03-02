ddd502886207a4cab4dfb593e9ae482446aae8b6
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-03-02 07:22:14 -0300

    fix(hide): hidde skills for auto deploy

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -7,7 +7,7 @@ function Skills({lang}) {
 
 
   return (
-    <Box w='100%' h='100dvh' display='grid' placeContent='center' className='aboutMe sliderMandatoryStart' hidden={false}>
+    <Box w='100%' h='100dvh' display='grid' placeContent='center' className='aboutMe sliderMandatoryStart' hidden={true}>
     <Box mx='30px' pb='5rem'>
         <Box display='flex' flexDir='row' mb='1rem' className='sliderMandatoryMobile'>
             <Heading fontSize={['5xl', '5xl', '5xl', '7xl']} fontWeight='thin'>{translations[lang].skills_title}</Heading>
